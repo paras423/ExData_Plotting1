b25926785c7aa2ec31126285ba0028e878e08950
paras423 <48555510+paras423@users.noreply.github.com>
2019-04-01 02:52:10 +0530

    Add files via upload
    Course Exploratory Data Analysis Week 1 Project

---
+++ plot1.R
@@ -0,0 +1,17 @@
+plot1<- function(){
+
+  
+  ## read data
+  powerdata <- read.table("./household_power_consumption.txt",header = TRUE, sep =";", na.strings="?", check.names=F, stringsAsFactors=F, comment.char="", quote='\"'   )
+
+  powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")  ##Date Conversion to another format 
+  powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)  ## Changing required data to numeric form
+  
+  
+
+  subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")  ##Selecting the data required
+  
+  dev.copy(png,"plot1.png",width= 480 ,height = 480)      ##For the png
+  hist(subsetdata$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+  dev.off()
+}
---
+++ plot2.R
@@ -0,0 +1,19 @@
+plot2 <- function(){
+ 
+  data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+  data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007")) ## Reads only selected data 
+  data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")     ##Changing the format
+  datetime <- paste(as.Date(data1$Date), data1$Time)     
+  data1$Datetime <- as.POSIXct(datetime)
+  dev.copy(png, file="plot2.png", height=480, width=480)
+  ## Plot 2
+  with(data1, {
+    plot(Global_active_power~Datetime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+  })
+  dev.off()
+  
+
+  
+
+}
